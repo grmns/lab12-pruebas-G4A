ef1100218c1a233a2f8363eb5cdf7ee6d2cb4a16
grmns <germanalanxd12@gmail.com>
2023-11-15 14:59:16 -0500

    create, update (vet)

--- src/main/java/com/tecsup/petclinic/webs/VetController.java
+++ src/main/java/com/tecsup/petclinic/webs/VetController.java
@@ -1,4 +1,60 @@
 package com.tecsup.petclinic.webs;
 
+import com.tecsup.petclinic.domain.PetTO;
+import com.tecsup.petclinic.entities.Pet;
+import com.tecsup.petclinic.exception.PetNotFoundException;
+import com.tecsup.petclinic.mapper.VetMapper;
+import com.tecsup.petclinic.domain.VetTO;
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.exception.VetNotFoundException;
+import com.tecsup.petclinic.services.VetService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
 public class VetController {
+
+    private final VetService vetService;
+    private final VetMapper mapper;
+
+    public VetController(VetService vetService, VetMapper mapper) {
+        this.vetService = vetService;
+        this.mapper = mapper;
+    }
+    @PostMapping(value = "/vets")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<VetTO> create(@RequestBody VetTO vetTO) {
+        Vet newVet = this.mapper.toVet(vetTO);
+        VetTO newVetTO = this.mapper.toVetTO(vetService.create(newVet));
+        return ResponseEntity.status(HttpStatus.CREATED).body(newVetTO);
+    }
+
+    @PutMapping(value = "/vets/{id}")
+    ResponseEntity<VetTO>  update(@RequestBody VetTO vetTO, @PathVariable Integer id) {
+
+        VetTO updateVetTO = null;
+
+        try {
+
+            Vet updateVet = vetService.findById(id);
+
+            updateVet.setFirstName(vetTO.getFirstName());
+            updateVet.setLastName(vetTO.getLastName());
+
+            vetService.update(updateVet);
+
+            updateVetTO = this.mapper.toVetTO(updateVet);
+
+        } catch (VetNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(updateVetTO);
+    }
+
 }
--- src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -1,7 +1,82 @@
 package com.tecsup.petclinic.webs;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.tecsup.petclinic.domain.VetTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Slf4j
 class VetControllerTest {
 
+    private static final ObjectMapper om = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void testCreateVet() throws Exception {
+        String FIRST_NAME = "German";
+        String LAST_NAME = "Sandoval";
+
+        VetTO newVetTO = new VetTO();
+        newVetTO.setFirstName(FIRST_NAME);
+        newVetTO.setLastName(LAST_NAME);
+
+        mockMvc.perform(post("/vets")
+                        .content(om.writeValueAsString(newVetTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(FIRST_NAME)))
+                .andExpect(jsonPath("$.lastName", is(LAST_NAME)));
+    }
+
+    @Test
+    void testUpdateVet() throws Exception {
+        String FIRST_NAME = "German";
+        String LAST_NAME = "Sandoval";
+
+        VetTO newVetTO = new VetTO();
+        newVetTO.setFirstName(FIRST_NAME);
+        newVetTO.setLastName(LAST_NAME);
+
+        mockMvc.perform(post("/vets")
+                        .content(om.writeValueAsString(newVetTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(FIRST_NAME)))
+                .andExpect(jsonPath("$.lastName", is(LAST_NAME)));
+
+        String vetFirstName = "Juan";
+        String vetLastName = "Perez";
+
+        VetTO updateVetTO = new VetTO();
+        updateVetTO.setFirstName(vetFirstName);
+        updateVetTO.setLastName(vetLastName);
+
+        ResultActions result = mockMvc.perform(put("/vets/{id}", 1)
+                        .content(om.writeValueAsString(updateVetTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(vetFirstName)))
+                .andExpect(jsonPath("$.lastName", is(vetLastName)));
+    }
 }
